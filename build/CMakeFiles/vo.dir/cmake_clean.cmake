c9d2ebcfad4b7a1e109fcf862bcb127de0e5c8e6
arunmadhusud <arunsreekuttan1996@gmail.com>
2024-08-01 19:58:03 -0400

    Added project files

---
+++ build/CMakeFiles/vo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vo.dir/LatLong-UTMconversion.cpp.o"
+  "CMakeFiles/vo.dir/LatLong-UTMconversion.cpp.o.d"
+  "CMakeFiles/vo.dir/vo_main.cpp.o"
+  "CMakeFiles/vo.dir/vo_main.cpp.o.d"
+  "vo"
+  "vo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
