8d0fdae15c833b323ffd2f5fe7f00cf65acaadac
arunmadhusud <arunsreekuttan1996@gmail.com>
2024-08-01 20:01:44 -0400

    Added project files

---
+++ build/CMakeFiles/vo.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/arun/my_github/visual_odometry/src/LatLong-UTMconversion.cpp" "CMakeFiles/vo.dir/src/LatLong-UTMconversion.cpp.o" "gcc" "CMakeFiles/vo.dir/src/LatLong-UTMconversion.cpp.o.d"
+  "/home/arun/my_github/visual_odometry/src/vo_main.cpp" "CMakeFiles/vo.dir/src/vo_main.cpp.o" "gcc" "CMakeFiles/vo.dir/src/vo_main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/vo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vo.dir/src/LatLong-UTMconversion.cpp.o"
+  "CMakeFiles/vo.dir/src/LatLong-UTMconversion.cpp.o.d"
+  "CMakeFiles/vo.dir/src/vo_main.cpp.o"
+  "CMakeFiles/vo.dir/src/vo_main.cpp.o.d"
+  "vo"
+  "vo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
